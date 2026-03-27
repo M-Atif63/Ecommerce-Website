71de6e558136e751a717cea34169fc0e8eb83b3e
M.Atif63 <muhammed.atif.ali.63@gmail.com>
2026-03-27 13:24:23 +0500

    Add Signup page

---
+++ auth.js
@@ -0,0 +1,35 @@
+import { initializeApp, getAuth, firebaseConfig, createUserWithEmailAndPassword, onAuthStateChanged } from "./firebase.js";
+
+const app = initializeApp(firebaseConfig)
+const auth = getAuth(app)
+console.log('app=>', app)
+console.log('auth=>', auth)
+
+var sbtn = document.getElementById("sbtn")
+sbtn.addEventListener("click", signup)
+
+async function signup() {
+  var semail = document.getElementById("semail")
+  var spassword = document.getElementById("spassword")
+  var message = document.getElementById("mess")
+
+  // else if(!)
+  createUserWithEmailAndPassword(auth, semail.value, spassword.value)
+    .then((userCredential) => {
+      const user = userCredential.user;
+      console.log("user=>", user.email)
+      message.innerText = "Account Created Successfully"
+      message.style.color="green"
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      message.innerText = "Please fill correct Email & Password";
+      message.style.color="red"
+      console.log("Error=>", errorMessage)
+    });
+    if(!spassword.value || !semail.value){
+      message.innerText = "Please fill All Fields"
+      return
+    }
+}
